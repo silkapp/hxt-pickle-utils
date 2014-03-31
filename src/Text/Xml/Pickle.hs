c5fcd45539a797db608a85f36ebef270c291cb2f
Erik Hesselink <hesselink@gmail.com>
2014-03-31 15:48:17 +0200

    Add documentation.

--- src/Text/Xml/Pickle.hs
+++ src/Text/Xml/Pickle.hs
@@ -1,19 +1,27 @@
-module Text.Xml.Pickle where
+-- | Some utility functions for using hxt picklers.
+module Text.Xml.Pickle
 
 import Control.Monad.Identity
 import Control.Category
 import Prelude hiding ((.))
 import Text.XML.HXT.Core
 
+-- | Convert a value to an XML string.
 toXML :: XmlPickler p => p -> String
 toXML = showPickled []
 
+-- | Parse a string containing xml to a value. On parse failure, will
+-- call `error`.
 fromXML :: XmlPickler a => String -> a
 fromXML = runIdentity . fromXMLM
 
+-- | Parse a string containing xml to a value, or `Nothing` if the
+-- parse fails.
 maybeFromXML :: XmlPickler a => String -> Maybe a
 maybeFromXML = fromXMLM
 
+-- | Parse a string containing xml to a value, or an error message on
+-- failure.
 eitherFromXML :: XmlPickler a => String -> Either String a
 eitherFromXML text =
   case runLA (removeAllWhiteSpace . xread) text of
